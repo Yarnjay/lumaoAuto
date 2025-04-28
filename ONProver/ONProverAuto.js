1ff2217cf47b41140bf5e672e0acc7f414edb088
Yarnjay <115167200+Yarnjay@users.noreply.github.com>
2025-04-28 22:01:21 +0800

    Update ONProverAuto.js

--- ONProver/ONProverAuto.js
+++ ONProver/ONProverAuto.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         ONProverAuto
 // @namespace    https://onprover.orochi.network/
-// @version      v20250428
+// @version      v20250428-10
 // @description  Happy day
 // @author       YuanJay
 // @match        https://onprover.orochi.network/*
